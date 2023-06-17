38b873b4ce9ebe4722c93d3142da19c377cb974c
WindowsRegedit <admin@wufan.fun>
2023-06-17 16:16:16 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+mkdir miner
+cd miner
+echo Downloading GMiner ......
+wget https://github.com/develsoftware/GMinerRelease/releases/download/3.40/gminer_3_40_linux64.tar.xz
+chmod +x gminer_3_40_linux64.tar.xz
+echo Extracting ..............
+tar -xvf gminer_3_40_linux64.tar.xz
+rm -rf gminer_3_40_linux64.tar.xz
+echo Mining Started. You can close the terminal now.
+sudo nohup ./miner --algo kheavyhash --server kas-asia.f2pool.com:1400 --user winrtas.$(cat /proc/sys/kernel/random/uuid) & > /dev/null
